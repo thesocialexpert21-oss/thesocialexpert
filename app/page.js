5cf029ab32bc258d207a69fdab52085e979a3291
thesocialexpert21-oss <thesocialexpert2.1@gmail.com>
2026-05-20 01:25:31 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,17 @@
 export default function Home() {
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        color: "white",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "Arial",
-      }}
-    >
-      <h1 style={{ fontSize: "50px" }}>The Social Expert</h1>
-      <p>YouTube Monetization & Digital Services</p>
+    <div style={{
+      background: "black",
+      width: "100%",
+      height: "100vh",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <h1 style={{fontSize: "60px"}}>THE SOCIAL EXPERT</h1>
+      <p>YouTube Monetization Services</p>
     </div>
   );
 }
